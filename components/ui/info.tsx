040a07305ee7fb6f87e55d73f10e442ec5699271
Tien <tien04100@gmail.com>
2023-10-18 20:48:29 +0700

    create cart feature

--- components/ui/info.tsx
+++ components/ui/info.tsx
@@ -1,15 +1,23 @@
-"use client"
+"use client";
 
-import { Product } from "@/types";
-import Currency from "./currency";
-import Button from "./button";
 import { ShoppingCart } from "lucide-react";
+
+import Currency from "@/components/ui/currency";
+import Button from "@/components/ui/button";
+import { Product } from "@/types";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart();
+
+  const onAddToCart = () => {
+    cart.addItem(data);
+  };
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -22,7 +30,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
       <div className="flex flex-col gap-y-6">
         <div className="flex items-center gap-x-4">
           <h3 className="font-semibold text-black">Size:</h3>
-          <div>{data?.size?.name}</div>
+          <div>{data?.size?.value}</div>
         </div>
         <div className="flex items-center gap-x-4">
           <h3 className="font-semibold text-black">Color:</h3>
@@ -33,11 +41,10 @@ const Info: React.FC<InfoProps> = ({ data }) => {
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-        <Button className="flex items-center gap-x-2">
-            Add To Cart
-            <ShoppingCart />
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
+          Add To Cart
+          <ShoppingCart size={20} />
         </Button>
-
       </div>
     </div>
   );
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -8,12 +8,14 @@ import Currency from "./currency";
 import { useRouter } from "next/navigation";
 import { MouseEventHandler } from "react";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCardProps {
   data: Product;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  const cart = useCart();
   const previewModal = usePreviewModal();
   const router = useRouter();
 
@@ -25,6 +27,11 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
     event.stopPropagation();
 
     previewModal.onOpen(data);
+  };
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    cart.addItem(data);
   };
 
   return (
@@ -47,7 +54,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-              onClick={() => {}}
+              onClick={onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
